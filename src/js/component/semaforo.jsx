43aea281fca017da7858dbd564a22c27e39dfc53
Diwoop <144156230+Diwoop@users.noreply.github.com>
2024-07-16 15:49:08 +0000

    semaforo acabado

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -1,30 +1,21 @@
 import React, {useState} from "react";
 
 export const Semaforo = () => {
-    const [color, setColor] = useState ('');
-    const [activarColor, setActivarColor] = useState('')
+    const [color, setColor] = useState ('red');
 
-    const setRojo = () => setColor('red')
-    const setAmarillo = () => setColor('yellow')
-    const setVerde = () => setColor('green')
-    console.log(color);
-
-    handleOnChange = () => {
-        
-    }
     
     return (
         <div className="container-fluid d-flex justify-content-center">
             <div className="container-fluid text-center bg-dark w-25 d-flex flex-column justify-content-between align-items-center"style={{ height: '500px' }}>
-                <div onClick={() => handleOnChange} className="bg-danger text-danger h-25 w-50 rounded-circle mt-4">
+                <div onClick={() => setColor('red')} className={`bg-danger text-danger h-25 w-50 rounded-circle mt-4 ${color === "red"? "glow":""}`}>
                     rojo
                 </div>
 
-                <div onClick={() => handleOnChange} className="bg-warning text-warning h-25 w-50 rounded-circle">
+                <div onClick={() => setColor('yellow') } className={`bg-warning text-warning h-25 w-50 rounded-circle ${color === "yellow"? "glow":""}`}>
                     amarillo
                 </div>
 
-                <div onClick={() => handleOnChange} className="bg-success text-success h-25 w-50 rounded-circle mb-4">
+                <div onClick={() => setColor('green') } className={`bg-success text-success h-25 w-50 rounded-circle mb-4 ${color === "green"? "glow":""}`}>
                     verde
                 </div>
             </div>
